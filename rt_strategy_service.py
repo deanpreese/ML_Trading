cdd95da5ed5708f1182db7b4f35ff10da2da9be4
deanpreese <deanpreese66@gmail.com>
2024-07-10 21:33:08 -0700

    changes

--- rt_strategy_service.py
+++ rt_strategy_service.py
@@ -28,9 +28,6 @@ models_three = []
 models_four = []
 
 
-def LoadModels():
-    strat_runs310 = ['e0b22b6f127348e99e3b4455eccce320', '9b32eacda2174ef0be6548229dc3d372', '0b97535148444f48923807597f6a5a43']      
-    return model_loader.load_models_by_run_ids(strat_runs310)
 
 def LoadModels(group_id, experiment_id, num_models):
     return model_loader.load_composite_models( experiment_id, num_models, group_id)    
@@ -47,57 +44,44 @@ def get_prediction(data_df, models):
 
 
 def get_v_prediction(data_df, models):
-    loaded_prediction = 0
-    loaded_percent = 0
-    loaded_sum_predicts = 0
+    
+    t_pct_cnt = 0
+    t_rtn = 0
+    t_agg = 0
+    t_agg_w = 0
+    t_ens = 0
     
     for m in range(len(models)):
-        percent, predict, sum_predicts = models[m].do_predict_v(data_df)
-        #print(f"Model V {percent}  {predict}  {sum_predicts}")
+        percentage_positive, return_predict, agg_predict, agg_weighted_predict = models[m].do_predict_v(data_df)
         
-        loaded_prediction += predict
-        loaded_percent += percent
-        loaded_sum_predicts += sum_predicts
+        if percentage_positive >= 0.5:
+            t_pct_cnt += 1
         
-    final_ave_pct = loaded_percent / len(models)
-    final_ave_predict = loaded_prediction / len(models)
-    final_ave_sum_predicts = loaded_sum_predicts / len(models)
+        t_rtn += return_predict
+        t_agg += agg_predict   
+        t_agg_w += agg_weighted_predict             
 
-    print(f"Final  V    {final_ave_predict}  {final_ave_sum_predicts}  {final_ave_pct} ")
+    rtn = t_rtn 
+    pct = t_pct_cnt/len(models)
+    agg = t_agg/len(models) 
+    agg_w = t_agg_w/len(models)
+
+    if pct > 0.5 and (  rtn > 0 ) :
+        t_ens = rtn    
+
+    if pct < 0.5 and ( rtn < 0 ) :
+        t_ens = rtn   
+
+    if pct == 0.5 and ( rtn > 0) :
+        t_ens = rtn   
+
+    if pct == 0.5 and ( rtn < 0 ) :
+        t_ens = rtn   
+
+    comp_predict = ((0.49 * rtn) + (0.53 * pct) + (0.44 * agg) + (0.42 * agg_w) + (0.46 * t_ens))/5
     
-    return final_ave_predict
+    return rtn, pct, agg, agg_w, t_ens, comp_predict
 
-
-def get_v2_prediction(data_df, models):
-    loaded_prediction = 0
-    loaded_percent = 0
-    loaded_sum_predicts = 0
-    
-    final_ave_predict = 0
-    
-    for m in range(len(models)):
-        percent, predict, sum_predicts = models[m].do_predict_v(data_df)
-        #print(f"Model V {percent}  {predict}  {sum_predicts}")
-        
-        loaded_prediction += predict
-        loaded_percent += percent
-        loaded_sum_predicts += sum_predicts
-        
-    final_ave_pct = loaded_percent / len(models)
-    calc_ave_predict = loaded_prediction / len(models)
-    final_ave_sum_predicts = loaded_sum_predicts / len(models)
-
-    final_ave_predict = calc_ave_predict[0]
-
-    if final_ave_sum_predicts[0] > 0 and final_ave_pct < 0.5:
-        final_ave_predict = 0
-
-    if final_ave_sum_predicts[0] < 0 and final_ave_pct > 0.5:
-        final_ave_predict = 0
-
-    print(f"Final  V2  {final_ave_predict}   {calc_ave_predict[0]}   {final_ave_pct}   {final_ave_sum_predicts[0]} ")
-    
-    return final_ave_predict
 
 
 def init_app():
@@ -109,7 +93,6 @@ def init_app():
        models_two = LoadModels(0, ["59"], 3)
        models_three = LoadModels(0, ["59"], 3)
        models_four = LoadModels(0, ["59"], 5)
-       
 
               
     @app.route('/predict-one', methods=['POST'])
@@ -130,49 +113,49 @@ def init_app():
     def predict_two():
         
         csv_data = BytesIO(request.data)
-        #column_names = ['time', 'SDLR310', 'SDBB91', 'SDKC91', 'SDKC9', 'ROC', 'ATR34', 'ATR32', 'ATR31', 'ATR3', 'ATR21', 'ATR2', 'RSI', 'STOK1', 'output', 'outputC', 'actual']
-        column_names = ['time', 'SDLR310', 'SDBB91', 'SDKC91', 'SDKC9', 'ROC', 'ATR33', 'ATR32', 'ATR31', 'ATR3', 'ATR21', 'ATR2', 'RSI', 'STOK1', 'output', 'outputC', 'actual']
+        column_names = ['time', 'SDLR310', 'SDBB91', 'SDKC91', 'SDKC9', 'ROC', 'ATR34', 'ATR32', 'ATR31', 'ATR3', 'ATR21', 'ATR2', 'RSI', 'STOK1', 'output', 'outputC', 'actual']
         
         data_df = pd.read_csv(csv_data, header=None, names=column_names)
         data_df.drop(columns=['time', 'actual', 'output', 'outputC'], inplace=True)
         
-        final_predict = get_v_prediction(data_df, models_two)
+        rtn, pct, agg, agg_w, t_ens, comp_predict = get_v_prediction(data_df, models_two)
         
-        loaded_prediction = final_predict
-        print(f"Predict 2 Model  {loaded_prediction}")            
-        return str(loaded_prediction)
+        
+        if pct > 0.5: 
+            pct = 1
+        else:
+            pct = -1    
+        
+        print(f"Predict pct Model  {pct}")            
+        return str(pct)
         
         
     @app.route('/predict-three', methods=['POST'])
     def predict_three():
         
         csv_data = BytesIO(request.data)
-        #column_names = ['time', 'SDLR310', 'SDBB91', 'SDKC91', 'SDKC9', 'ROC', 'ATR34', 'ATR32', 'ATR31', 'ATR3', 'ATR21', 'ATR2', 'RSI', 'STOK1', 'output', 'outputC', 'actual']
-        column_names = ['time', 'SDLR310', 'SDBB91', 'SDKC91', 'SDKC9', 'ROC', 'ATR33', 'ATR32', 'ATR31', 'ATR3', 'ATR21', 'ATR2', 'RSI', 'STOK1', 'output', 'outputC', 'actual']        
+        column_names = ['time', 'SDLR310', 'SDBB91', 'SDKC91', 'SDKC9', 'ROC', 'ATR34', 'ATR32', 'ATR31', 'ATR3', 'ATR21', 'ATR2', 'RSI', 'STOK1', 'output', 'outputC', 'actual']        
         
         data_df = pd.read_csv(csv_data, header=None, names=column_names)
         data_df.drop(columns=['time', 'actual', 'output', 'outputC'], inplace=True)
 
-        final_predict = get_v2_prediction(data_df, models_three)
-        
-        loaded_prediction = final_predict
-        print(f"Predict 3 Model  {loaded_prediction}")            
-        return str(loaded_prediction)
+        rtn, pct, agg, agg_w, t_ens, comp_predict = get_v_prediction(data_df, models_two)
+           
+        print(f"Predict agg_w Model  {agg}")            
+        return str(agg)
     
     @app.route('/predict-four', methods=['POST'])
     def predict_four():
         
         csv_data = BytesIO(request.data)
-        #column_names = ['time', 'SDLR310', 'SDBB91', 'SDKC91', 'SDKC9', 'ROC', 'ATR34', 'ATR32', 'ATR31', 'ATR3', 'ATR21', 'ATR2', 'RSI', 'STOK1', 'output', 'outputC', 'actual']
-        column_names = ['time', 'SDLR310', 'SDBB91', 'SDKC91', 'SDKC9', 'ROC', 'ATR33', 'ATR32', 'ATR31', 'ATR3', 'ATR21', 'ATR2', 'RSI', 'STOK1', 'output', 'outputC', 'actual']        
+        column_names = ['time', 'SDLR310', 'SDBB91', 'SDKC91', 'SDKC9', 'ROC', 'ATR34', 'ATR32', 'ATR31', 'ATR3', 'ATR21', 'ATR2', 'RSI', 'STOK1', 'output', 'outputC', 'actual']        
         
         data_df = pd.read_csv(csv_data, header=None, names=column_names)
         data_df.drop(columns=['time', 'actual', 'output', 'outputC'], inplace=True)
 
-        loaded_prediction = 0
-        loaded_prediction = get_v2_prediction(data_df, models_four)
-        print(f"Predict 4 Model  {loaded_prediction}")            
-        return str(loaded_prediction)    
+        rtn, pct, agg, agg_w, t_ens, comp_predict = get_v_prediction(data_df, models_two)
+        print(f"Predict comp_predict Model  {comp_predict}")            
+        return str(comp_predict)    
             
         
         
