5a872e593ad004242f1d76ed9471f8a2df6b178b
Dean <deanpreese66@gmail.com>
2024-09-14 10:57:16 -0700

    mmmm

--- mcnn.py
+++ mcnn.py
@@ -39,6 +39,7 @@ class DCNN:
        
         self.checkpoint_model = os.path.join(self.checkpoint_dir, 'mcnn_model.keras')
         self.trained_model = os.path.join(self.trained_dir, 'mcnn_model.keras')
+        self.dot_img_file = os.path.join(self.checkpoint_dir, 'mcnn.png')
 
 
         self.drop_out = 0.2
@@ -46,7 +47,6 @@ class DCNN:
         self.initializer = GlorotUniform(seed=42)
         
     def build_sub_model_a(self, inputs):        
-        
         x = Conv1D(filters=64, kernel_size=2, activation='relu', kernel_initializer=self.initializer)(inputs)
         x = LSTM(64, kernel_regularizer=self.l2_reg, activation='relu', return_sequences=True, kernel_initializer=self.initializer)(x)
         x = Conv1D(filters=32, kernel_size=3, activation='relu', kernel_initializer=self.initializer)(x)
@@ -59,14 +59,12 @@ class DCNN:
         x = LSTM(32, kernel_regularizer=self.l2_reg, activation='relu', kernel_initializer=self.initializer)(x)
         x = Dropout(self.drop_out)(x)
         x = Dense(16, activation='relu', kernel_regularizer=self.l2_reg, kernel_initializer=self.initializer)(x)        
-        
         return x
     
     def build_sub_model_b(self, inputs):        
-        
-        x = Conv1D(filters=64, kernel_size=2, activation='relu', kernel_initializer=self.initializer)(inputs)       
+        x = Conv1D(filters=64, kernel_size=3, activation='relu', kernel_initializer=self.initializer)(inputs)       
         x = LSTM(64, kernel_regularizer=self.l2_reg, activation='relu', return_sequences=True, kernel_initializer=self.initializer)(x)
-        x = Conv1D(filters=32, kernel_size=3, activation='relu', kernel_initializer=self.initializer)(x)
+        x = Conv1D(filters=32, kernel_size=2, activation='relu', kernel_initializer=self.initializer)(x)
         x = LSTM(32, kernel_regularizer=self.l2_reg, activation='relu', return_sequences=True, kernel_initializer=self.initializer)(x)
         x = Conv1D(filters=64, kernel_size=1, activation='relu', kernel_initializer=self.initializer)(x)
         x = Dropout(self.drop_out)(x)
@@ -75,10 +73,36 @@ class DCNN:
         x = Bidirectional(LSTM(16,name="BIC2", kernel_regularizer=self.l2_reg, kernel_initializer=self.initializer))(x)
         x = Dropout(self.drop_out)(x)
         x = Dense(16, activation='relu', kernel_regularizer=self.l2_reg, kernel_initializer=self.initializer)(x)
-        
-        
         return x
 
+
+    def build_sub_model_c(self, inputs):        
+        x = Conv1D(filters=64, kernel_size=4, activation='relu', kernel_initializer=self.initializer)(inputs)       
+        #x = LSTM(64, kernel_regularizer=self.l2_reg, activation='relu', return_sequences=True, kernel_initializer=self.initializer)(x)
+        x = Conv1D(filters=32, kernel_size=1, activation='relu', kernel_initializer=self.initializer)(x)
+        #x = LSTM(32, kernel_regularizer=self.l2_reg, activation='relu', return_sequences=True, kernel_initializer=self.initializer)(x)
+        x = Conv1D(filters=64, kernel_size=1, activation='relu', kernel_initializer=self.initializer)(x)
+        #x = Dropout(self.drop_out)(x)
+        x = Bidirectional(LSTM(32,name="BIC", kernel_regularizer=self.l2_reg, return_sequences=True, kernel_initializer=self.initializer))(x)
+        #x = Dropout(self.drop_out)(x)
+        x = Bidirectional(LSTM(16,name="BIC2", kernel_regularizer=self.l2_reg, kernel_initializer=self.initializer))(x)
+        x = Dropout(self.drop_out)(x)
+        x = Dense(16, activation='relu', kernel_regularizer=self.l2_reg, kernel_initializer=self.initializer)(x)
+        return x
+
+    def build_sub_model_d(self, inputs):        
+        x = Conv1D(filters=32, kernel_size=4, activation='relu', kernel_initializer=self.initializer)(inputs)       
+        #x = LSTM(64, kernel_regularizer=self.l2_reg, activation='relu', return_sequences=True, kernel_initializer=self.initializer)(x)
+        x = Conv1D(filters=16, kernel_size=1, activation='relu', kernel_initializer=self.initializer)(x)
+        #x = LSTM(32, kernel_regularizer=self.l2_reg, activation='relu', return_sequences=True, kernel_initializer=self.initializer)(x)
+        x = Conv1D(filters=32, kernel_size=1, activation='relu', kernel_initializer=self.initializer)(x)
+        #x = Dropout(self.drop_out)(x)
+        x = Bidirectional(LSTM(32,name="BIC", kernel_regularizer=self.l2_reg, return_sequences=True, kernel_initializer=self.initializer))(x)
+        #x = Dropout(self.drop_out)(x)
+        x = Bidirectional(LSTM(16,name="BIC2", kernel_regularizer=self.l2_reg, kernel_initializer=self.initializer))(x)
+        x = Dropout(self.drop_out)(x)
+        x = Dense(16, activation='relu', kernel_regularizer=self.l2_reg, kernel_initializer=self.initializer)(x)
+        return x
 
     def build_model(self, input_shape):
         inputs = Input(shape=input_shape)
@@ -86,19 +110,23 @@ class DCNN:
         a = self.build_sub_model_a(inputs)
         b = self.build_sub_model_b(inputs)
         c = self.build_sub_model_a(inputs)
-        d = self.build_sub_model_b(inputs)
+        d = self.build_sub_model_c(inputs)
+        e = self.build_sub_model_b(inputs)
+        #f = self.build_sub_model_c(inputs)
+        #ave_output = Average()([ a, b, c, d, e, f ])
 
-
-        ave_output = Average()([ a, b, c, d ])
+        ao_1 = MultiHeadAttention(num_heads=4, key_dim=4, kernel_regularizer=self.l2_reg)(a, b)
+        #ao_2 = MultiHeadAttention(num_heads=4, key_dim=16, kernel_regularizer=self.l2_reg)(ao_1, c)
+        #ave_output = Average()([ a, b, c, d, e ])
         
-        outputs = Dense(1)(ave_output)  
+        ave_output = ao_1
+
+        outputs = Dense(1 )(ave_output)  
         model = Model(inputs=inputs, outputs=outputs)
         model.compile(optimizer=Adam(learning_rate=0.001), loss='mse', metrics=['mae', tf.keras.metrics.R2Score()])
         model.summary(expand_nested=True,show_trainable=True)
         
-            
-        dot_img_file = os.path.join(self.checkpoint_dir, 'mcnn.png')
-        tf.keras.utils.plot_model(model, to_file=dot_img_file, show_shapes=True)
+        tf.keras.utils.plot_model(model, to_file=self.dot_img_file, show_shapes=True)
     
         
         print(" ")
