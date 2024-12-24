e2ff97ff9ca84faa596ceb6cd68af85cc9eb0e26
Dean Reese <deanpreese66@gmail.com>
2024-12-24 11:43:12 -0800

    models

--- model_cnn_lg.py
+++ model_cnn_lg.py
@@ -19,7 +19,7 @@ from keras.callbacks import EarlyStopping, ReduceLROnPlateau
 
 from ml_model.data_func import split_three_ways
 from ml_model.k_model_base import K_MODEL_BASE
-from ml_model.k_model_base import FFTLayer
+from ml_model.k_model_base import FFTLayer, FFTOrRFTLayer
 
 import ml_model.feature_filter as feature_filter
 
@@ -38,11 +38,15 @@ class CNN_LG (K_MODEL_BASE):
 
     def build_model_hfft(self, inputs):
         # Apply FFT using the custom layer
-        h = FFTLayer()(inputs)
+        
+        #h = FFTLayer()(inputs)
         #h = FFTLayer()(h)
         
-        h2 = FFTLayer()(inputs)
+        #h2 = FFTLayer()(inputs)
         #h2 = FFTLayer()(h2)
+        
+        h = FFTOrRFTLayer(use_rft=True, return_magnitude=True)(inputs)
+        h2 = FFTOrRFTLayer(use_rft=True, return_magnitude=True)(inputs)
         
         
         h = Conv1D(filters=64, kernel_size=8, activation='relu', kernel_initializer=self.initializer, kernel_regularizer=self.l2_reg)(h)
@@ -114,7 +118,6 @@ class CNN_LG (K_MODEL_BASE):
         ao2 = LayerNormalization()(ao2)
         ao2 = Dropout(0.2)(ao2)
 
-        
         h = (0.5 *ao) + (0.5 *ao2)
 
         # LSTM for temporal sequence modeling
@@ -143,18 +146,16 @@ class CNN_LG (K_MODEL_BASE):
         
         hx = self.build_model_hx(inputs)
         hft = self.build_model_hfft(inputs)
-        ave_output = 0.5*hx + 0.5*hft
         
+        ave_out = 0.5*hx + 0.5*hft
         
-        """
-        
+        #tsr        
         tm1 = Dense(64, kernel_regularizer=self.l2_reg, activation='tanh',kernel_initializer=self.initializer)(ave_out)
         tm2 = Dense(64, kernel_regularizer=self.l2_reg, activation='sigmoid', kernel_initializer=self.initializer)(ave_out)
         tm_r = Dense(64, kernel_regularizer=self.l2_reg, activation='relu', kernel_initializer=self.initializer)(ave_out)
-        final_ave_out = Average()([tm1,tm2, tm_r])  
-        """
-        
-        final_ave_out = ave_output
+        #final_ave_out = Average()([tm1,tm2, tm_r])  
+
+        final_ave_out = ave_out
         
         final_out = Dense(32, activation='relu', kernel_regularizer=self.l2_reg, kernel_initializer=self.initializer)(final_ave_out)
                 
@@ -179,11 +180,11 @@ def main():
     #col_filter = feature_filter.model_x_3070_imp_slim
     #col_filter = feature_filter.model_x_3070_comp
     
-    model = CNN_LG('cnn_lg_lucky13_all')
+    model = CNN_LG('cnn_lg_lucky13_all_ave')
     X_train, X_val, X_test, y_train, y_val, y_test,  X_oos, y_oos, input_shape = model.process_data_split(datafile[1], datafile[0], col_filter)
     
     history_out, y_pred = model.train_model(input_shape, X_train, X_test, y_train, y_test, X_val, y_val, 1000 )
-    best_model = tf.keras.models.load_model(model.checkpoint_model, custom_objects={"FFTLayer": FFTLayer})
+    best_model = tf.keras.models.load_model(model.checkpoint_model, custom_objects={"FFTLayer": FFTLayer, "FFTOrRFTLayer": FFTOrRFTLayer})
     model.evaluate_finished_model(best_model, X_val, X_test, y_train, y_val, y_test,  X_oos, y_oos)
     
             
